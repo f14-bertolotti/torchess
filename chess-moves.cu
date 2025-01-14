7e2f2b0a314f862cf49bbb14096c68121f32cf24
Francesco Bertolotti <f14.bertolotti@gmail.com>
2025-01-14 17:33:13 +0100

    few added some pawn logic + minors

--- chess-moves.cu
+++ chess-moves.cu
@@ -1,18 +1,61 @@
 #pragma once
 #include <torch/extension.h>
 #include "chess-consts.h"
-#include "clamp.cu"
 
-__device__ unsigned char kingside_castle(
+__device__ unsigned char standard_action(
     int env,
     torch::PackedTensorAccessor64<long , 1, torch::RestrictPtrTraits> players,
     torch::PackedTensorAccessor64<long , 2, torch::RestrictPtrTraits> boards,
     torch::PackedTensorAccessor64<long , 2, torch::RestrictPtrTraits> actions
 ) {
+    // performs a standard action
+    // returns 0 if everything is ok
+    // returns 1 if the action was a standard action but the conditions were not met
+
+    const char PLAYER_PAWN = 6 * players[env] + WHITE_PAWN;
+
+    const bool is_standard_action = (
+        (actions[env][0] >= 0) * (actions[env][0] < 8) * // 0 <= action source row <= 7
+        (actions[env][1] >= 0) * (actions[env][1] < 8) * // 0 <= action source col <= 7
+        (actions[env][2] >= 0) * (actions[env][2] < 8) * // 0 <= action target row <= 7
+        (actions[env][3] >= 0) * (actions[env][3] < 8) * // 0 <= action target col <= 7
+        (actions[env][4] == 0)                           // no special action
+    );
+
+    const bool is_source_fill = boards[actions[env][0] * 8 + actions[env][0]] != EMPTY;
+    
+    const bool is_pawn_moving = (
+        (actions[env][0] * 8 + actions[env][1] == PLAYER_PAWN           ) * // pawn is in the right position
+        (actions[env][0] == actions[env][2] (players[env]==BLACK?+1:-1) ) * // pawn is moving one row forward
+        (actions[env][1] == actions[env][3]                             ) * // pawn is not changing col
+        (boards[env][actions[env][2] * 8 + actions[env][3]] == EMPTY    )   // target is empty
+    );
+    
+    const bool is_pawn_capturing = (
+        (boards[env][actions[env][0] * 8 + actions[env][1]] == PLAYER_PAWN   ) * // pawn is in the right position
+        (actions[env][0] == actions[env][2] (players[env]==(BLACK ? +1 : -1))) * // pawn is moving one row forward
+        (abs(actions[env][1] - actions[env][3]) == 1                         ) * // pawn is moving one col to the side
+        (( // target is black or white
+            (boards[env][actions[env][2] * 8 + actions[env][3]] > 0  ) *
+            (boards[env][actions[env][2] * 8 + actions[env][3]] < 6  )   // target is white
+        ) + 
+        (
+            (boards[env][actions[env][2] * 8 + actions[env][3]] > 6  ) * 
+            (boards[env][actions[env][2] * 8 + actions[env][3]] < 12 )   // target is black
+        ))
+    );
+
+}
+
+__device__ unsigned char kingside_castle(
+    int env,
+    torch::PackedTensorAccessor64<long , 1, torch::RestrictPtrTraits> players,
+    torch::PackedTensorAccessor64<long , 2, torch::RestrictPtrTraits> boards ,
+    torch::PackedTensorAccessor64<long , 2, torch::RestrictPtrTraits> actions
+) {
     // performs kingside castling action
     // returns 0 if everything is ok
     // returns 1 if the action was a kingside castling but the conditions were not met
-
 
     const unsigned char PLAYER_KING = players[env] * 6 + WHITE_KING;
     const unsigned char PLAYER_ROOK = players[env] * 6 + WHITE_ROOK;
@@ -51,7 +94,7 @@ __device__ unsigned char kingside_castle(
 }
 
 __device__ unsigned char queenside_castle(
-     int env,
+    int env,
     torch::PackedTensorAccessor64<long , 1, torch::RestrictPtrTraits> players,
     torch::PackedTensorAccessor64<long , 2, torch::RestrictPtrTraits> boards,
     torch::PackedTensorAccessor64<long , 2, torch::RestrictPtrTraits> actions
@@ -74,7 +117,7 @@ __device__ unsigned char queenside_castle(
         (actions[env][1] == 0    ) * // action source empty
         (actions[env][2] == 0    ) * // action target empty
         (actions[env][3] == 0    ) * // action target empty
-        (special == QUEEN_CASTLE ) * // queenside castling action
+        (special == QUEEN_CASTLE )   // queenside castling action
     );
 
     const bool is_action_ok = ( 
@@ -90,14 +133,15 @@ __device__ unsigned char queenside_castle(
         (count_attacks(env, rook_target, 2, players, boards) == 0 )   // rook target is not in check
     );
 
-    boards[env][rook_target] = is_queenside_castle * is_queenside_ok ? PLAYER_ROOK : boards[env][rook_target];
-    boards[env][king_target] = is_queenside_castle * is_queenside_ok ? PLAYER_KING : boards[env][king_target];
-    boards[env][king_source] = is_queenside_castle * is_queenside_ok ? EMPTY       : boards[env][king_source];
-    boards[env][rook_side]   = is_queenside_castle * is_queenside_ok ? EMPTY       : boards[env][rook_side];
-    boards[env][rook_source] = is_queenside_castle * is_queenside_ok ? EMPTY       : boards[env][rook_source];
+    boards[env][rook_target] = is_queenside_castle * is_action_ok ? PLAYER_ROOK : boards[env][rook_target];
+    boards[env][king_target] = is_queenside_castle * is_action_ok ? PLAYER_KING : boards[env][king_target];
+    boards[env][king_source] = is_queenside_castle * is_action_ok ? EMPTY       : boards[env][king_source];
+    boards[env][rook_side]   = is_queenside_castle * is_action_ok ? EMPTY       : boards[env][rook_side];
+    boards[env][rook_source] = is_queenside_castle * is_action_ok ? EMPTY       : boards[env][rook_source];
 
-    return is_queenside_castle * (!is_action_ok)
+    return is_queenside_castle * (!is_action_ok);
 }
+
     
 
 __global__ void step_kernel(
@@ -111,6 +155,7 @@ __global__ void step_kernel(
     // It also updates the rewards and the dones
     // The function is in-place, it modifies input/actions/dones/rewards in place
 
+    const int env = blockIdx.x * blockDim.x + threadIdx.x;
     const unsigned char OPPONENT_PAWN   = ((players[env] + 1) % 2 * 6) + WHITE_PAWN;
     const unsigned char OPPONENT_KNIGHT = ((players[env] + 1) % 2 * 6) + WHITE_KNIGHT;
     const unsigned char OPPONENT_BISHOP = ((players[env] + 1) % 2 * 6) + WHITE_BISHOP;
@@ -125,16 +170,15 @@ __global__ void step_kernel(
     const unsigned char PLAYER_QUEEN  = players[env] * 6 + WHITE_QUEEN;
     const unsigned char PLAYER_KING   = players[env] * 6 + WHITE_KING;
 
-    const bool  WHITE_TURN      = players[0] == 0;
-    const bool  BLACK_TURN      = players[0] == 1;
+    const bool  WHITE_TURN = players[0] == 0;
+    const bool  BLACK_TURN = players[0] == 1;
 
-    const int env = blockIdx.x * blockDim.x + threadIdx.x;
     const unsigned char src = actions[env][0] * 8 + actions[env][1];
     const unsigned char tgt = actions[env][2] * 8 + actions[env][3];
     const unsigned char spc = actions[env][4];
 
-    const unsigned char  kingside_castling_result =  kingside_castling(env, players, boards, actions);
-    const unsigned char queenside_castling_result = queenside_castling(env, players, boards, actions);
+    const unsigned char  kingside_castling_result =  kingside_castle(env, players, boards, actions);
+    const unsigned char queenside_castling_result = queenside_castle(env, players, boards, actions);
 
     // // check if action indexes are inside the board
     // if (actions[env][0] < 0 || actions[env][0] >= 8 || // starting row
--- clamp.cu
+++ clamp.cu
@@ -1,3 +1,4 @@
+#pragma once
 
 __device__ __inline__ int clamp(long left, long right, long val) {
     // This function clamps the value between left and right, both included
