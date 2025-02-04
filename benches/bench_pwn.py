55a5aefb68ab6ae7d4923033b1b6077a794c2c17
Francesco Bertolotti <f14.bertolotti@gmail.com>
2025-02-04 15:13:40 +0100

    fixed benchmark

--- benches/bench_pwn.py
+++ benches/bench_pwn.py
@@ -1,7 +1,8 @@
 import time
 import chess
 import torch
-import pysrc
+from pysrc.utils import chs2pwn
+from torchess import step
 
 
 def main():
@@ -9,21 +10,20 @@ def main():
 
     actions = torch.randint(0, 8, (1000, batch_size, 5), device="cuda:0", dtype=torch.int)
     
-    board, player = pysrc.utils.chs2pwn(chess.Board())
+    board, player = chs2pwn(chess.Board())
     board, player = board.repeat(actions.size(0),1), player.repeat(actions.size(0))
 
-    pysrc.step(board, actions[:,0], player)  # (warmup) Run once before timing
+    reward = torch.empty(board.size(0), 2, device="cuda:0", dtype=torch.float)
+    dones  = torch.empty(board.size(0), device="cuda:0", dtype=torch.bool)
+    step(board, actions[:,0], player, dones, reward)  # (warmup) Run once before timing
     
     starttime = time.time()
     for i in range(actions.size(0)):
-        pysrc.step(board, actions[i], player)
+        step(board, actions[i], player, dones, reward)
     endtime = time.time() - starttime
  
     print(f"Time taken: {endtime:.5f} seconds")
     print(f"Average time per step: {endtime/100:.5f} seconds")
-
-   
-    
 
 if __name__ == '__main__':
     main()
